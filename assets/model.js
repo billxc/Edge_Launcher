0501cd97e38165cd1ac30f954bd14805c9a563b3
Xiaochen <wxc9312@gmail.com>
2024-02-23 01:11:42 +0800

    tmp

--- assets/model.js
+++ assets/model.js
@@ -1,20 +1,45 @@
-DEFAULT_PROFILES =
+export const DEFAULT_PROFILES =
   [
     {
       "name": "Default",
       "exePath": "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe",
       "appDir": "c:\\tmp",
+      "selectedChannel": "stable",
       "extraParams": "--no-first-run  --disable-features=msImplicitSignin,msSeamlessWebToBrowserSignIn"
     },
     {
       "name": "Edge Launcher 2",
       "exePath": "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe",
       "appDir": "",
+      "selectedChannel": "stable",
+      "extraParams": "--no-first-run  --disable-features=msImplicitSignin,msSeamlessWebToBrowserSignIn"
+    },
+    {
+      "name": "Edge Launcher 3",
+      "exePath": "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe",
+      "appDir": "",
+      "selectedChannel": "stable",
+      "extraParams": "--no-first-run  --disable-features=msImplicitSignin,msSeamlessWebToBrowserSignIn"
+    },
+    {
+      "name": "Edge Launcher 4",
+      "exePath": "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe",
+      "appDir": "",
+      "selectedChannel": "stable",
+      "extraParams": "--no-first-run  --disable-features=msImplicitSignin,msSeamlessWebToBrowserSignIn"
+    },
+    {
+      "name": "Edge Launcher 5",
+      "exePath": "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe",
+      "appDir": "",
+      "selectedChannel": "stable",
       "extraParams": "--no-first-run  --disable-features=msImplicitSignin,msSeamlessWebToBrowserSignIn"
     }
   ];
 
-function getProfileParams(profile) {
+
+
+export function getProfileParams(profile) {
   if (profile.appDir == "") {
     return profile.extraParams
   } else {
